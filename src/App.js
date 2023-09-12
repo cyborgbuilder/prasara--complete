afe7b3456e52b39a575e3fda1c00c2d5532852bb
cyborgbuilder <isuruud8@gmail.com>
2023-09-12 16:08:26 +0530

    basename removed

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
       {/* <Navbar />
       {component} */}
 
-      <BrowserRouter basename='/home'>
+      <BrowserRouter >
       <Navbar />
       <SocialBooth />
         <Routes>
